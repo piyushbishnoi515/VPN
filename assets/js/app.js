e9294be9b554a7c6782d5f9c6254ab691cc5bca6
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-04-15 15:45:09 +0530

    slick slider autoplay

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,39 @@ const menulist = document.querySelector(".menulist");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("showlist");
 })
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed:300,
+    autoplay: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});

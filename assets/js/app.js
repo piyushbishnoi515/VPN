0ea9b06f04dff7d0e54c355e5df2f297bb1d002d
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-04-18 15:24:59 +0530

    flex wrap update in button

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,16 +29,11 @@ $('.slider').slick({
             breakpoint: 600,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 2,
+                  dots: false,
             }
         }
+        
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
         // instead of a settings object
